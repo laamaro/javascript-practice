3f7891cbe5eb91d4ba5bad03d618c7ec8d272c89
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-08 14:17:04 -0300

    practice for basic operators lecture

--- JS-Fundamentals-Part1/01-Fundaments/script.js
+++ JS-Fundamentals-Part1/01-Fundaments/script.js
@@ -1,12 +1,20 @@
 // Values and Variables
 const country = 'Brasil';
 const continent = 'South America';
-let population = 200000000;
+let populationBr = 214000000;
 
 // Data Type
 const isIsland = false;
 const language = 'Portuguese';
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+console.log(typeof isIsland, typeof populationBr, typeof country, typeof language);
+
+// Basic Operators
+let halfPopulation = populationBr / 2;
+console.log(halfPopulation);
+console.log(populationBr++);
+let populationFinland = 6000000;
+console.log(populationBr > populationFinland);
+const avgPopulation = 33000000;
+console.log(populationBr < avgPopulation);
+const description = country + " is in " + continent + ", and its " + populationBr + " people that speak " + language;
+console.log(description);
