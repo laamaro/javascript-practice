9fc73143151657f510e093b088c8ef073463afb1
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-07 22:16:54 -0300

    finish practice for data types practice

---
+++ JS-Fundamentals-Part1/01-Fundaments/script.js
@@ -0,0 +1,15 @@
+// Values and Variables:
+let country = 'Brasil';
+let continent = 'South America';
+let population = 200000000;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// Data Type:
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
