7f722a6a7cc0837cb297dd76e334e4a62797427b
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-14 00:04:22 -0300

    lectures practice and challenge 2

--- JS-Fundamentals-Part1/01-Challenge/script.js
+++ JS-Fundamentals-Part1/01-Challenge/script.js
@@ -21,9 +21,21 @@ const massJohn = 92;
 const heightJohn = 1.95;
 
 const BMIMark = massMark / (heightMark * heightMark);
-const BMTJohn = massJohn / (heightJohn * heightJohn);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
 
-console.log(BMIMark, BMTJohn);
+console.log(BMIMark, BMIJohn);
 
-const markHigherBMI = BMIMark > BMTJohn;
+const markHigherBMI = BMIMark > BMIJohn;
 console.log(markHigherBMI);
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+ */
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
--- JS-Fundamentals-Part1/01-Fundaments/script.js
+++ JS-Fundamentals-Part1/01-Fundaments/script.js
@@ -1,7 +1,7 @@
 // Values and Variables
 const country = 'Brasil';
 const continent = 'South America';
-let populationBr = 214000000;
+let populationBr = 214;
 
 // Data Type
 const isIsland = false;
@@ -12,13 +12,20 @@ console.log(typeof isIsland, typeof populationBr, typeof country, typeof languag
 let halfPopulation = populationBr / 2;
 console.log(halfPopulation);
 console.log(populationBr++);
-let populationFinland = 6000000;
+let populationFinland = 60;
 console.log(populationBr > populationFinland);
-const avgPopulation = 33000000;
+const avgPopulation = 33;
 console.log(populationBr < avgPopulation);
-let description = country + " is in " + continent + ", and its " + populationBr + " people that speak " + language;
+let description = country + " is in " + continent + ", and its " + populationBr + " million people that speak " + language;
 console.log(description);
 
 // Strings and Template Literals
 description = `${country} is in ${continent}, and its ${populationBr} people that speak ${language}!`;
 console.log(description);
+
+// Taking decisions: if/else statements
+if (populationBr > avgPopulation) {
+  console.log(`${country}'s population is above avarage!`);
+} else {
+  console.log(`${country}'s population is ${avgPopulation - populationBr} million bellow the avarage...`);
+}
