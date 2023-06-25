f42d7935e362fb00386b79b2f83fd256776eb033
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-24 21:08:40 -0300

    practice for function lecture

---
+++ JS-Fundamentals-Part2/01-Practices/script.js
@@ -0,0 +1,7 @@
+// Functions
+function describeCountry(country, population, capitalCity) {
+  const description = `${country} has ${population} million people and its capital city is ${capitalCity}.`
+  return description;
+}
+
+console.log(describeCountry('Brazil', 214, 'Brasília'));
