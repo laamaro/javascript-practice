d0ca295295050c1a6d68bd42f14dbe149eddfc2f
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-27 17:11:24 -0300

    selecting element with DOM

--- 03-DOM-Manipulation/01-Project-Guess-My-Number/script.js
+++ 03-DOM-Manipulation/01-Project-Guess-My-Number/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value;
