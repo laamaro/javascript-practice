75f66242cb7e307211d63ed72fd942d837ae71a6
Laura Amaro <lauraamaro1804@gmail.com>
2023-06-29 00:57:41 -0300

    change page style for when the guess is correct

--- 03-DOM-Manipulation/01-Project-Guess-My-Number/script.js
+++ 03-DOM-Manipulation/01-Project-Guess-My-Number/script.js
@@ -10,8 +10,12 @@ document.querySelector('.check').addEventListener('click', () => {
 
   if (!guess) {
     document.querySelector('.message').textContent = "No number inputed 😢 Try Again!";
+
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = "Correct Number!! 🎉";
+    document.querySelector('body').style.backgroundColor = '#23d438';
+    document.querySelector('.number').style.width = '40rem';
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = "Guess is too high!!";
@@ -21,6 +25,7 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('.message').textContent = "You lost the game 😢"
       document.querySelector('.score').textContent = 0;
     }
+
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = "Guess is too low!!;"
