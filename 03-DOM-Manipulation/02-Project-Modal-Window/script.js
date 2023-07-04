dfeb560249a9569403ebc19affb99851265944d8
Laura Amaro <lauraamaro1804@gmail.com>
2023-07-04 00:45:24 -0300

    closing modal and refactor code

--- 03-DOM-Manipulation/02-Project-Modal-Window/script.js
+++ 03-DOM-Manipulation/02-Project-Modal-Window/script.js
@@ -5,8 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', () => {
     modal.classList.remove('hidden');
-  })
-}
+    overlay.classList.remove('hidden');
+  });
+
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
