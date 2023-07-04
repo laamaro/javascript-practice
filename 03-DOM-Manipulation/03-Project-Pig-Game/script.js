86d7e5194ac46cda6bd03fa09bf692cc094d17e3
Laura Amaro <lauraamaro1804@gmail.com>
2023-07-04 19:54:59 -0300

    select the element that will be used for the third game

--- 03-DOM-Manipulation/03-Project-Pig-Game/script.js
+++ 03-DOM-Manipulation/03-Project-Pig-Game/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// Select elements we'll use
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const newGame = document.querySelector('.btn--new');
+const rollDice = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
+const dice = document.querySelector('.dice');
+
+// Starting the game
+score0El.textContent = 0;
+score1El.textContent = 0;
+dice.classList.add('hidden');
