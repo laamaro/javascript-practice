682994c105cef265325741825629141e7bb9219b
Laura Amaro <lauraamaro1804@gmail.com>
2023-07-04 21:10:05 -0300

    start implementing the rolling dice logic

--- 03-DOM-Manipulation/03-Project-Pig-Game/script.js
+++ 03-DOM-Manipulation/03-Project-Pig-Game/script.js
@@ -3,10 +3,14 @@
 // Select elements we'll use
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.getElementById('current--1');
 const newGame = document.querySelector('.btn--new');
 const rollDice = document.querySelector('.btn--roll');
 const hold = document.querySelector('.btn--hold');
 const dice = document.querySelector('.dice');
+
+let currentScore = 0
 
 // Starting the game
 score0El.textContent = 0;
@@ -22,5 +26,13 @@ rollDice.addEventListener('click', () => {
   dice.classList.remove('hidden');
   dice.src = `dice-${diceNumber}.png`;
 
-  // Check for value 1: switch player
+  if (diceNumber !== 1) {
+    // Add number to current score
+    currentScore += diceNumber;
+    current0.textContent = currentScore; // This will be changed
+
+  } else {
+    // Switch player
+
+  }
 })
